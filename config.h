c9adb9432435f63a82398cdd3d7d1f2aeca68bab
Apig Sharbo <apigsharbo@gmail.com>
2021-04-19 20:22:03 +0400

    If we don't want any window class to be treated in a special way, we need to initialize rules with at least one element.

--- config.h
+++ config.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
 	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
 	/* { "firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
